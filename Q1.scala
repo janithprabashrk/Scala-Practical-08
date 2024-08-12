c7180bb03bda391661b8ebf01319361cfe41724f
R.K. Janith Prabash <janithprabash944ugc@gmail.com>
2024-08-12 21:38:38 +0530

    Create Q1.scala

---
+++ Q1.scala
@@ -0,0 +1,31 @@
+object CaesarCipher {
+
+    def encrypt(text : String, shift : Int) : String = {
+        text.map(chr =>
+            if(chr.isLetter) {
+                val base = if (chr.isUpper) 'A' else 'a'
+                ((chr - base + shift) % 26 + base).toChar
+            }
+            else chr
+        )
+    } 
+
+    def decrypt(text : String, shift : Int) : String = {
+        encrypt(text, 26 - shift)
+    }
+
+    def funcSelector(text : String, shift : Int, function: (String, Int) => String) : String = {
+        function(text, shift)
+    }
+
+    def main(args : Array[String]) : Unit = {
+        val text = "Scala Practicals"
+        val shift = 3
+        val encrypted = funcSelector(text, shift, encrypt)
+        val decrypted = funcSelector(encrypted, shift, decrypt)
+
+        println(" certain text : " + text)
+        println("Encrypted text : " + encrypted)
+        println("Decrypted text : " + decrypted)
+    }
+}
